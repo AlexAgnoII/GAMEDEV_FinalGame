9e86f1b7e0f6695d5899ce9e72565d7f85ea5c4f
AlexAgnoII <3alex23@gmail.com>
2018-11-24 23:52:32 +0800

    World generation development

---
+++ Crossy_Road_Game/Assets/Scripts/GameScripts/TerrainData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Terrain Data", menuName = "Terrain Data")]
+public class TerrainData : ScriptableObject {
+    [SerializeField] private GameObject terrain;
+    [SerializeField] private int maxInSuccession;
+    [SerializeField] private int minInSucession;
+
+
+
+    
+    public GameObject getTerrain()
+    {
+        return this.terrain;
+    }
+
+    public int getMaxInSuccession()
+    {
+        return this.maxInSuccession;
+    }
+
+    public int getMinInSuccession()
+    {
+        return this.minInSucession;
+    }
+
+
+}
---
+++ Crossy_Road_Game/Assets/Scripts/GameScripts/TerrainGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour {
+
+    [SerializeField] private GameObject parentLocation;
+    [SerializeField] private const int MAX_TERRAIN_COUNT = 20;    
+    [SerializeField] private List<TerrainData> terrainData_List = new List<TerrainData>();
+    [SerializeField] private Queue<GameObject> currentTerrainsQueue = new Queue<GameObject>();
+    
+
+    private Vector3 currentPosition = new Vector3(0, 0, 0);
+
+
+
+    
+    void Start () {
+
+        /*for(int i = 0; i < MAX_TERRAIN_COUNT; i++)
+        {
+            SpawnTerrain();
+        }*/
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            SpawnTerrain();
+        }
+    }
+
+    private void SpawnTerrain()
+    {
+        GameObject terrain, terrainRemove;
+        int terrainIndex = Random.Range(0, terrainData_List.Count);
+        int terrainInSuccession = Random.Range(0, terrainData_List[terrainIndex].getMaxInSuccession());
+
+        
+
+        for(int i = 0; i < terrainInSuccession; i++)
+        {
+            terrain = Instantiate(terrainData_List[terrainIndex].getTerrain(), currentPosition, Quaternion.identity);
+            terrain.transform.parent = parentLocation.transform;
+            currentTerrainsQueue.Enqueue(terrain);
+
+            //If the current terrain spawned exceeds the max terrain count, remove the oldest.
+            if (currentTerrainsQueue.Count > MAX_TERRAIN_COUNT)
+            {
+                terrainRemove = currentTerrainsQueue.Dequeue();
+                Destroy(terrainRemove);
+            }
+
+            currentPosition.x++;
+        }
+    }
+}
