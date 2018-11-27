4d8826caa9f20a16c001bfc90a802da2ad2e23b4
AlexAgnoII <3alex23@gmail.com>
2018-11-27 23:14:05 +0800

    Update 10
    - Lowered player's sphere collider area from 0.5, to 0.4 (to fix bug of not following logs since its hitting obstacles on the near grass beside it)
    - added prefabs with obstacles (collision not yet working)
    - adjusted camera angle.

--- Crossy_Road_Game/Assets/Scripts/GameScripts/CameraScript.cs
+++ Crossy_Road_Game/Assets/Scripts/GameScripts/CameraScript.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour {
 
     [SerializeField] GameObject player;
-    [SerializeField] private Vector3 offSet = new Vector3(-2.5f, 7f , -6f);
-    [SerializeField] private Vector3 offSet_nope = new Vector3(0f, 7f, -6f);
+    [SerializeField] private Vector3 offSet = new Vector3(-2.5f, 7f, -6f);
     [SerializeField] private float smoothness = 0.15f;
 
     private void Awake()
